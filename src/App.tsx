ea99ac1941fcd5dacd276bcb3351ae5050e26c9f
firesquid06 <jdeiss06@gmail.com>
2023-05-25 16:53:44 -0500

    fix issues with excess imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import { useState } from "react";
 import "./App.css";
 import { FaTwitter, FaGithub } from "react-icons/fa";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div>
