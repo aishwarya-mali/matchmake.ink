252d87ca4396dbfe81a0cedeb09ef9de49de89e0
firesquid06 <jdeiss06@gmail.com>
2023-05-31 15:12:35 -0500

    add palette.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
+import { ViteToml } from "vite-plugin-toml";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), ViteToml()],
   server: {
     port: 8008,
   },
