8578c3ac9341a385c1f28d895303624f64cc2adb
firesquid <jdeiss06@gmail.com>
2023-05-30 23:15:01 -0500

    add error checking for anon key

--- src/backend/client.ts
+++ src/backend/client.ts
@@ -7,4 +7,10 @@ import env from "../../env.json";
 const url = env.SUPABASE_URL;
 const anonKey = env.SUPABASE_ANON_KEY;
 
+if (url === undefined || anonKey === undefined) {
+  throw new Error(
+    "Supabase URL or Anon Key is undefined. Please fill out env.json with them. If you do not have these, Please contact FireSquid#8882 on discord."
+  );
+}
+
 export const databaseClient = createClient(url, anonKey);
