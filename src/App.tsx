be9be4571c86edd64214d9a326129d36cc370e57
firesquid06 <jdeiss06@gmail.com>
2023-06-02 17:58:59 -0500

    make routing work with login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,26 @@
 import "./index.css";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { useSession } from "./backend/session";
 
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 
 export default function App() {
+  const session = useSession();
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/" element={<Home />} />
-          <Route path="/team" element={<p>team</p>} />
-          <Route path="/profile" element={<p>profile</p>} />
-          <Route path="/settings" element={<p>settings</p>} />
-        </Routes>
-      </BrowserRouter>
+      {session ? (
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/team" element={<p>team</p>} />
+            <Route path="/profile" element={<p>profile</p>} />
+            <Route path="/settings" element={<p>settings</p>} />
+          </Routes>
+        </BrowserRouter>
+      ) : (
+        <Login />
+      )}
     </>
   );
 }
