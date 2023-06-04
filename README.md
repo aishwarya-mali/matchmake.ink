4fed89fbd30da76417c953f22569a1ceeea565d9
firesquid06 <jdeiss06@gmail.com>
2023-06-03 21:15:58 -0500

    switch to using a .env file

--- README.md
+++ README.md
@@ -45,6 +45,10 @@ Once you've made an accepted PR, feel free to DM me on discord (FireSquid#8882)
 1. Clone the repo with whatever you want
 2. Open the project in your editor of choice
 3. Run `npm install`
-4. Create your env.json file. See `src/backend/client.ts` for info on what this should contain
+4. Create your .env file:
+	- As of 6-3-2023, your .env file should look like:
+	- ```VITE_SUPABASE_URL="https://zdxeeicdysjerhtalkbi.supabase.co"
+		VITE_SUPABASE_ANON_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpkeGVlaWNkeXNqZXJodGFsa2JpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODUzOTU2NDQsImV4cCI6MjAwMDk3MTY0NH0.GYlVhTcsm9Kr-tqASRGwZUinEo_7np_bTbgh3-GAF0I"
+		```
 5. Run `npm run dev`
 6. Choose an issue to start working on. If you're new, look for stuff marked with `good-first-issue`
--- src/backend/client.ts
+++ src/backend/client.ts
@@ -1,18 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
-// if you're wondering why we're using env.json instead of .env, it's because I'm a moron and couldn't figure out how to get .env to work with Vite
-// if you know how to fix this, feel free to do so
-import env from "../../env.json";
+// having erros here? Make sure you've filled out your .env with the VITE_SUPABASE_URL and VITE_SUPABASE_ANON_KEY. See README.md for more info.
 
-// having errors here? Try filling out env.json. If you have no clue what to do, contact FireSquid#8882 on discord.
-// as of 31-05-2023, my env.json looks like:
-/* {
-  "SUPABASE_URL": "https://zdxeeicdysjerhtalkbi.supabase.co",
-  "SUPABASE_ANON_KEY": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpkeGVlaWNkeXNqZXJodGFsa2JpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODUzOTU2NDQsImV4cCI6MjAwMDk3MTY0NH0.GYlVhTcsm9Kr-tqASRGwZUinEo_7np_bTbgh3-GAF0I"
-} */
-// feel free to just copy and paste that and see if it works
-const url = env.SUPABASE_URL;
-const anonKey = env.SUPABASE_ANON_KEY;
+const url = import.meta.env.VITE_SUPABASE_URL;
+const anonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 if (url === undefined || anonKey === undefined) {
   throw new Error(
