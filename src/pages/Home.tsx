ccbd82343022adda2461f546bb24781b47f857f8
firesquid06 <jdeiss06@gmail.com>
2023-06-04 17:39:42 -0500

    fix error

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,31 +1,7 @@
-import { useSession } from "../backend/session";
-import { useCurrentProfile } from "../backend/profile";
-import { UserProfile } from "../components/user-profile/UserProfile";
-
 export default function Home() {
-  const session = useSession();
-  const [profile] = useCurrentProfile(session);
-
-  if (profile === null || session === null) {
-    return (
-      <div className="page">
-        <p>
-          Wait... You shouldn't be here! Please <a href="/login">log in</a>.
-        </p>
-      </div>
-    );
-  }
-
   return (
     <main className="page">
-      <UserProfile
-        discordTag={profile.discord_tag || ""}
-        profilePictureUrl={profile.avatar_url || ""}
-        friendCode={profile.friend_code || ""}
-        team={profile.team || ""}
-        twitterHandle={profile.twitter_handle || ""}
-        sendouPage={profile.sendou_page || ""}
-      />
+      <p>Homepage</p>
     </main>
   );
 }
